1b615c0743c29188f07ca03fa15fcea5c6ec2575
pratikkadam00 <pratikadam00@gmail.com>
2023-09-26 21:42:08 +0530

    day 1 reverse a array

--- ReverseString.js
+++ ReverseString.js
@@ -1,6 +1,17 @@
-// ascending sort
+//reverse a string
 let oldArr = [12, 3, 43, 2, 34, 78, 7, 9, 4]
 let length = oldArr.length
+
+for (let i = 0; i < Math.floor(length / 2); i++) {
+    let temp = oldArr[i];
+    oldArr[i] = oldArr[length - 1 - i];
+    oldArr[length - 1 - i] = temp;
+}
+
+console.log(oldArr);
+
+
+// ascending sort
 for (let i = 0; i <= length; i++) {
     for (let j = 0; j <= length - 1; j++) {
         if (oldArr[j] > oldArr[j + 1]) {
