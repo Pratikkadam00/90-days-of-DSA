e73a93bf293167837a2460aa3f345f0e3e968a0e
pratikkadam00 <pratikadam00@gmail.com>
2023-09-28 21:35:24 +0530

    week-1

---
+++ Week-1/queue.js
@@ -0,0 +1,3 @@
+// A queue is a linear data structure that follows the First - In - First - Out(FIFO) principle,
+// meaning that the first element added to the queue is the first one to be removed.
+// Here's an example of implementing a queue in JavaScript:
