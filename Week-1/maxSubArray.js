95085fbf5e43828bc2d1b5e4a378ed03d7cdeac7
pratikkadam00 <pratikadam00@gmail.com>
2023-09-29 19:39:49 +0530

    week-1

--- Week-1/maxSubArray.js
+++ Week-1/maxSubArray.js
@@ -12,4 +12,12 @@ function maxSubarraySum(arr) {
 
 const arr = [-2, 1, -3, 4, -1, 2, 1, -5, 4];
 const maxSum = maxSubarraySum(arr);
-console.log("Maximum Subarray Sum is: " + maxSum);
+console.log("Maximum Subarray Sum is: " + maxSum); //output 6
+
+// Explanation:
+
+// maxSubarraySum function uses Kadane's algorithm to find the maximum subarray sum.
+// It initializes maxSum and currentSum with the first element of the array.
+// It iterates through the array, updating currentSum to be the maximum of the current element or the sum of currentSum and the current element.
+// It updates maxSum with the maximum of maxSum and currentSum in each iteration.
+//     Finally, it returns the maxSum
