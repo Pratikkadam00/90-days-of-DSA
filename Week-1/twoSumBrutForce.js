be879f0c3740a40fc7b69f964ac8f6c38368564f
pratikkadam00 <pratikadam00@gmail.com>
2023-09-29 19:42:08 +0530

    week-1

--- Week-1/twoSumBrutForce.js
+++ Week-1/twoSumBrutForce.js
@@ -13,3 +13,10 @@ const nums = [2, 7, 11, 15];
 const target = 9;
 const result = twoSum(nums, target);
 console.log("Two Sum indices are: " + result); // output 0,1
+
+// Explanation:
+
+// twoSum function takes an array of numbers and a target value.
+// It uses a nested loop to check all possible pairs of numbers in the array.
+// If a pair is found whose sum equals the target, it returns the indices of those numbers.
+// If no such pair is found, it returns null.
