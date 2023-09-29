17386c91fa5b517786496db375e7e29699b12576
pratikkadam00 <pratikadam00@gmail.com>
2023-09-29 19:44:00 +0530

    week-1

--- Week-1/mergeTwoSortedArr.js
+++ Week-1/mergeTwoSortedArr.js
@@ -22,3 +22,12 @@ const nums1 = [1, 3, 5];
 const nums2 = [2, 4, 6];
 const mergedArray = mergeSortedArrays(nums1, nums2);
 console.log("Merged Sorted Array: " + mergedArray); // output 1,2,3,4,5,6
+
+// Explanation:
+
+// mergeSortedArrays function takes two sorted arrays, nums1 and nums2.
+// It uses two pointers(i for nums1 and j for nums2) to iterate through both arrays.
+// It compares the elements at the pointers and adds the smaller element to the merged array.
+// The pointers are then moved accordingly.
+// After the loop, if there are remaining elements in either nums1 or nums2, they are added to the merged array using concat.
+//     Finally, the merged array is returned.
